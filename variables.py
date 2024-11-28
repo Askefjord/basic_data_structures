7897fd9b51e78d49326fb7dd4961ac66d1e141a0
Askefjord <kriwjar_gubicky@icloud.com>
2024-11-28 20:56:57 +0300

    dev variable script

---
+++ variables.py
@@ -0,0 +1,8 @@
+hwCount = 12
+hoursCount = 1.5
+courseName = 'Python'
+timeForExercise = hwCount/hoursCount
+print('Курс:', courseName, '\n'
+        'Всего задач:', hwCount, '\n'
+        'Затрачено часов:', hoursCount, '\n'
+        'Среднее время выполнения:', timeForExercise)
