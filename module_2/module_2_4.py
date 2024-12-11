f52accfe3137a663c9c5c32375a077ca3b4d3bcb
Askefjord <kriwjar_gubicky@icloud.com>
2024-12-11 19:55:41 +0300

    edit module_2_4

--- module_2/module_2_4.py
+++ module_2/module_2_4.py
@@ -2,20 +2,21 @@ num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
 prime = []
 n_prime = []
-x = 0
-cnt = 0
 
 for i in num:
-    for j in range(1, i + 1):
-        x = i % j
-        if x == 0:
-            cnt += 1
-            if cnt == 2:
-                prime.append(i)
+    is_prime = True
+    if i < 2:
+        is_prime = False
+        continue
 
-            
-            
-        #else:
-            #n_prime.append(i)
+    for j in range(2, int(i**0.5)+1):
+       if i % j == 0:
+           is_prime = False
+           break
+    if is_prime:
+        prime.append(i)
+    else:
+        n_prime.append(i)
+
 print(prime)
 print(n_prime)
