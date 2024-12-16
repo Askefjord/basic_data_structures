f28a526db961db85f693498955b69a38fcfddd3e
Askefjord <kriwjar_gubicky@icloud.com>
2024-12-16 22:49:47 +0300

    redev module_2_6

--- module_2/module_2_6.py
+++ module_2/module_2_6.py
@@ -2,26 +2,15 @@ import random
 rn = random.randrange(3,21)
 pairs = []
 result = []
-
-def get_uninum(numbers):
-    unique = []
-    for num in numbers:
-        if num not in unique:
-            unique.append(num)
-
-    for i in range(len(numbers)):
-        for j in range(len(unique)):
-            if numbers[i][1] == unique[j]:
-                break
-
-    return unique
-
-
 ran = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-for i in list(ran):
-    for j in range(2,20):
-        pairs.append([i,j])
-    ran.remove(i)
+r = 0
+while r < 18:
+    for i in ran:
+        for j in ran:
+            pairs.append([i,j+1])
+        ran.remove(i)
+        r = i
+        break
 
 for i in pairs:
     if rn % sum(i) == 0:
