0245577d979f5b1228c11833b2b095b8b0938ed1
Askefjord <kriwjar_gubicky@icloud.com>
2024-12-17 21:55:57 +0300

    redev module_2_6

--- module_2/module_2_6.py
+++ module_2/module_2_6.py
@@ -1,8 +1,9 @@
 import random
-rn = random.randrange(3,21)
+rnum = random.randrange(3,21)
 pairs = []
 result = []
-ran = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+ran = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+nr = []
 r = 0
 while r < 18:
     for i in ran:
@@ -12,9 +13,19 @@ while r < 18:
         r = i
         break
 
+# for k in range(len(ran)):
+#     for i in ran:
+#         for j in ran:
+#             if j not in nr:
+#                 pairs.append([i,j+1])
+#         nr.append(i)
+#         break
+# print(pairs)
 for i in range(len(pairs)):
-    if rn % sum(pairs[i]) == 0:
+    if rnum % sum(pairs[i]) == 0:
         x = pairs[i]
         result.append(x)
-print(rn)
-print(*result)
+
+result = sum(result, [])
+print(f'Random number is: {rnum}')
+print('Your code: ', *result)
