8924a12d971f0a258c11dc1b3ee56b9c84086a75
Askefjord <kriwjar_gubicky@icloud.com>
2024-12-20 20:33:48 +0300

    complete module_2_6

--- module_2/module_2_6.py
+++ module_2/module_2_6.py
@@ -1,33 +1,28 @@
 import random
 rnum = random.randrange(3,21)
-pairs = []
 result = []
-ran = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
-nr = []
-r = 0
-while r < 18:
-    for i in ran:
-        for j in ran:
-            pairs.append([i,j+1])
-        ran.remove(i)
-        r = i
-        break
+plist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 
-# for k in range(len(ran)):
-#     for i in ran:
-#         for j in ran:
-#             if j not in nr:
-#                 pairs.append([i,j+1])
-#         nr.append(i)
-#         break
-# print(pairs)
+def get_unipairs(pairlist):
+    pairs = list(pairlist)
+    upairs = []
+    r = 0
+    l = len(pairlist)
+    while r < l:
+        for i in pairs:
+            for j in pairs:
+                upairs.append([i,j+1])
+            pairs.remove(i)
+            r = i
+            break
+    return upairs
 
-for i in range(len(pairs)):
-    if rnum % sum(pairs[i]) == 0:
-        x = pairs[i]
+unipairs = get_unipairs(plist)
+for i in range(len(unipairs)):
+    if rnum % sum(unipairs[i]) == 0:
+        x = unipairs[i]
         result.append(x)
 
-# result = ''.join(str(num) for sublist in pairs for num in sublist)
 result = sum(result, [])
 print(f'Random number is: {rnum}')
 print('Your code: ', *result)
