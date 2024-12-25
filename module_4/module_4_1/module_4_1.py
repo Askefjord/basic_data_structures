dd41f7e98ba471b36bb66f1cef5689db81d95097
Askefjord <kriwjar_gubicky@icloud.com>
2024-12-25 19:36:43 +0300

    add module_4_1

---
+++ module_4/module_4_1/module_4_1.py
@@ -0,0 +1,8 @@
+from true_div import divide as tdiv
+from fake_div import divide as fdiv
+
+print(tdiv(25,5))
+print(tdiv(4,0))
+
+print(fdiv(16,8))
+print(fdiv(0,0))
---
+++ module_4/module_4_1/true_div.py
@@ -0,0 +1,7 @@
+import math
+
+def divide(first, second):
+    try:
+        return first / second
+    except ZeroDivisionError:
+        return math.inf
