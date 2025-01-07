eaf68c9b8a34632bed1525c3ebfc732bad018c38
a5ke4@rd <kriwjar_gubicky@icloud.com>
2025-01-07 23:47:08 +0300

    Extended module_5_2

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -3,9 +3,17 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    # Module_5_2 "Dunder Methods" ↓ #############
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'\nName is: {self.name}\nNumber of floors: {self.number_of_floors}'
+    #############################################
+
     def go_to(self, floor):
         if floor > self.number_of_floors:
-            print('This floor does not exist') 
+            print('This floor does not exist')
         else:
             for num in range(1, floor+1):
                 print(num)
@@ -13,3 +21,7 @@ class House:
 some_moll = House('Some Moll', 15)
 some_moll.go_to(17)
 some_moll.go_to(5)
+
+# ↓ Module_5_2 "Dunder Methods"
+print(some_moll)
+print(len(some_moll))
