dd3a0aa2ba04051ca56539c08e7f3f31f10cfa4a
a5ke4j@rd <kriwjar_gubicky@icloud.com>
2025-01-08 23:20:35 +0300

    working on module_5_3

--- module_5/module_5_1.py
+++ module_5/module_5_1.py
@@ -11,6 +11,42 @@ class House:
         return f'\nName is: {self.name}\nNumber of floors: {self.number_of_floors}'
     #############################################
 
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors + other.number_of_floors
+        else:
+            return self.number_of_floors + other
+
+    def __radd__(self, other):
+        return self.__add__(other)
+
+    def __iadd__(self, other):
+        return self.__add__(other)
+
     def go_to(self, floor):
         if floor > self.number_of_floors:
             print('This floor does not exist')
@@ -19,9 +55,31 @@ class House:
                 print(num)
 
 some_moll = House('Some Moll', 15)
+aquapark = House('Aquapark', 10)
+
 some_moll.go_to(17)
 some_moll.go_to(5)
 
 # ↓ Module_5_2 "Dunder Methods"
 print(some_moll)
 print(len(some_moll))
+
+# ↓ Module_5_3 "Overdrive Methods"
+print('\n')
+print(some_moll == aquapark)
+print(some_moll + 10)
+print(some_moll)
+
+print(some_moll == aquapark)
+
+some_moll += 10
+print(some_moll)
+
+aquapark = 10 + aquapark
+print(aquapark)
+
+print(some_moll > aquapark)
+print(some_moll >= aquapark)
+print(some_moll < aquapark)
+print(some_moll <= aquapark)
+print(some_moll != aquapark)
