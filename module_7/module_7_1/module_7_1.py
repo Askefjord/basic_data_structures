f12114393d7cab88715cd1103b822eb7e67de948
a5ke4j@rd <kriwjar_gubicky@icloud.com>
2025-01-23 23:18:08 +0300

    add module_7_1

---
+++ module_7/module_7_1/module_7_1.py
@@ -0,0 +1,36 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}\n'
+
+
+class Shop:
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        with open(self.__file_name, 'r') as products_file:
+            return products_file.read()
+
+    def add(self, *products):
+        for product in products:
+
+            if str(product) in self.get_products():
+                print(f'Product {product.name} is already in the store')
+                continue
+
+            with open(self.__file_name, 'a') as product_file:
+                product_file.write(str(product))
+
+
+if __name__ == '__main__':
+    s1 = Shop()
+    p1 = Product('Potato', 50.5, 'Vegetables')
+    p2 = Product('Spaghetti', 3.4, 'Groceries')
+    p3 = Product('Potato', 5.5, 'Vegetables')
+    print(p2)  # __str__
+    s1.add(p1, p2, p3)
+    print(s1.get_products())
