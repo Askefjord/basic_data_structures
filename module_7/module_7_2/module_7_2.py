88071b53e88380422bc4d38151b46eb5dd0e3e35
a5ke4j@rd <kriwjar_gubicky@icloud.com>
2025-01-28 23:22:00 +0300

    redev module_7_2

--- module_7/module_7_2/module_7_2.py
+++ module_7/module_7_2/module_7_2.py
@@ -1,21 +1,19 @@
+def custom_write(file_name, strings):
+    strings_position = {}
+    file = open(file_name, 'w', encoding='utf-8')
+
+    for text in range(len(strings)):
+        strings_position[tuple([text + 1, file.tell()])] = strings[text]
+        file.write(f'{strings[text]}\n')
+    file.close()
+
+    return strings_position
+
 info = [
     'Text for tell.',
     'Используйте кодировку utf-8.',
     'Because there are 2 languages!',
     'Спасибо!'
 ]
-
-
-def custom_write(file_name, strings):
-    strings_position = {}
-    file = open(file_name, 'a', encoding='utf-8')
-
-    for text in range(len(strings)):
-        strings_position[tuple([text+1, file.tell()])] = strings[text]
-        file.write(f'{strings[text]}\n')
-    file.close()
-
-    return strings_position
-
 
 print(custom_write("output.txt", info))
